de62c5675b2c3c6b82a0bd2619bda62813bcab68
ZicBusiness <zeroinfinitocriativo@gmail.com>
2023-03-03 20:42:31 -0300

    Mod1

---
+++ js/header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+    <header>
+    <h1>Bela Gil<p className="r">&reg;</p></h1>
+    </header>
+    );
+    }
+    
+    ReactDOM.render(<Header />, document.getElementById("header_js"));
+    
